36ee630a9a88b98fb09a29cda012a82d1cb87ad6
micklq <574078710@qq.com>
2017-08-03 17:43:35 +0800

    201708031743

--- MSNet.WebAdmin/Global.asax.cs
+++ MSNet.WebAdmin/Global.asax.cs
@@ -18,7 +18,7 @@ namespace MSNet.WebAdmin
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+           // WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
